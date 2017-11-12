7071e454e16d5ea3936744480a519c7490fbb01f
Haed Hutner <headhunter105@hotmail.com>
2017-11-12 12:03:50 +0200

    Fix for Town Ruin missing No answer

--- src/main/java/com/atherys/towns/commands/town/TownRuinCommand.java
+++ src/main/java/com/atherys/towns/commands/town/TownRuinCommand.java
@@ -14,6 +14,7 @@ import org.spongepowered.api.command.args.CommandContext;
 import org.spongepowered.api.command.spec.CommandSpec;
 import org.spongepowered.api.entity.living.player.Player;
 import org.spongepowered.api.text.Text;
+import org.spongepowered.api.text.format.TextColors;
 
 import javax.annotation.Nullable;
 
@@ -43,11 +44,12 @@ public class TownRuinCommand extends TownsSimpleCommand {
 
     private void ruinTown( Player player, Town town ) {
         Question ruin = Question.of( Text.of("Are you sure you want to destroy your town? Doing so will eject all residents and unclaim all plots.") )
-                .addAnswer( Question.Answer.of( Text.of("Yes"), player1 -> {
+                .addAnswer( Question.Answer.of( Text.of( TextColors.GREEN, "Yes" ),player1 -> {
                     if ( town.getStatus().equals( TownStatus.CAPITAL ) ) {
                         TownMessage.warn( player, "You cannot destroy the capital of a nation!");
                     } else town.ruin();
                 }))
+                .addAnswer( Question.Answer.of( Text.of( TextColors.RED, "No" ), player1 -> {} ) )
                 .build();
         ruin.pollBook( player );
     }
