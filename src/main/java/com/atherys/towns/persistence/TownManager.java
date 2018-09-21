3f4502b1809191cc8d377475b7bfd33e857db5ce
Haed Hutner <mvvasilev@outlook.com>
2018-09-21 23:31:17 +0300

    change createTownFromPlot to createTownFromSelection

--- src/main/java/com/atherys/towns/persistence/TownManager.java
+++ src/main/java/com/atherys/towns/persistence/TownManager.java
@@ -2,9 +2,9 @@ package com.atherys.towns.persistence;
 
 import com.atherys.core.database.mongo.MorphiaDatabaseManager;
 import com.atherys.towns.AtherysTowns;
-import com.atherys.towns.model.Plot;
 import com.atherys.towns.model.Resident;
 import com.atherys.towns.model.Town;
+import com.flowpowered.math.vector.Vector2d;
 import java.util.Optional;
 
 public class TownManager extends MorphiaDatabaseManager<Town> {
@@ -13,7 +13,7 @@ public class TownManager extends MorphiaDatabaseManager<Town> {
         super(AtherysTowns.getDatabase(), AtherysTowns.getLogger(), Town.class);
     }
 
-    public Optional<Town> createTownFromPlot(Plot plot, Resident mayor) {
+    public Optional<Town> createTownFromSelection(Vector2d A, Vector2d B, Resident mayor) {
         // TODO: Implement
         return Optional.empty();
     }
