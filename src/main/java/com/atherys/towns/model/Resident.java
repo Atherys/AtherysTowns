fece35f0b03c84c7c80d82f23eac189a28daad51
Miroslav Vasilev <miroslav.vasilev@scalefocus.com>
2019-01-11 14:56:49 +0200

    Annotate some more

--- src/main/java/com/atherys/towns/model/Resident.java
+++ src/main/java/com/atherys/towns/model/Resident.java
@@ -2,10 +2,13 @@ package com.atherys.towns.model;
 
 import com.atherys.core.db.SpongeIdentifiable;
 import com.atherys.towns.api.Actor;
+import org.hibernate.annotations.GenericGenerator;
 
 import javax.annotation.Nonnull;
 import javax.persistence.CascadeType;
+import javax.persistence.Column;
 import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
 import javax.persistence.Id;
 import javax.persistence.JoinColumn;
 import javax.persistence.ManyToOne;
@@ -15,6 +18,9 @@ import java.util.UUID;
 public class Resident implements SpongeIdentifiable, Actor {
 
     @Id
+    @GeneratedValue(generator = "UUID")
+    @GenericGenerator(name = "UUID", strategy = "org.hibernate.id.UUIDGenerator")
+    @Column(name = "id", updatable = false, nullable = false)
     private UUID uuid;
 
     private String name;
--- src/main/java/com/atherys/towns/persistence/PermissionRepository.java
+++ src/main/java/com/atherys/towns/persistence/PermissionRepository.java
@@ -3,7 +3,6 @@ package com.atherys.towns.persistence;
 import com.atherys.core.db.AtherysRepository;
 import com.atherys.towns.AtherysTowns;
 import com.atherys.towns.model.PermissionNode;
-import org.slf4j.Logger;
 
 public class PermissionRepository extends AtherysRepository<PermissionNode, Long> {
     protected PermissionRepository() {
