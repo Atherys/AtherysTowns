4ac82e8913ae91cefdc54d55a4fa7fb7abd762a5
CodeStripper <brentgerstenschlager@gmail.com>
2020-07-22 23:41:14 -0400

    forgot to add command class.

---
+++ src/main/java/com/atherys/towns/command/town/TownPayDebtCommand.java
@@ -0,0 +1,22 @@
+package com.atherys.towns.command.town;
+
+import com.atherys.core.command.PlayerCommand;
+import com.atherys.core.command.annotation.Aliases;
+import com.atherys.core.command.annotation.Description;
+import com.atherys.core.command.annotation.Permission;
+import com.atherys.towns.AtherysTowns;
+import org.spongepowered.api.command.CommandException;
+import org.spongepowered.api.command.CommandResult;
+import org.spongepowered.api.command.args.CommandContext;
+import org.spongepowered.api.entity.living.player.Player;
+
+@Aliases("paydebt")
+@Description("Deletes your town.")
+@Permission("atherystowns.town.debt.pay")
+public class TownPayDebtCommand implements PlayerCommand {
+    @Override
+    public CommandResult execute(Player src, CommandContext args) throws CommandException {
+        AtherysTowns.getInstance().getTownFacade().payTownDebt(src);
+        return CommandResult.success();
+    }
+}
