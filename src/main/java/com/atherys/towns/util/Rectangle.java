65d853c51f7e937a965618c09483d0973228837c
Aaron Foley <aaron.s.foley@gmail.com>
2020-07-28 09:16:41 +1000

    Removed unneeded public

--- src/main/java/com/atherys/towns/util/Rectangle.java
+++ src/main/java/com/atherys/towns/util/Rectangle.java
@@ -3,22 +3,22 @@ package com.atherys.towns.util;
 import com.flowpowered.math.vector.Vector2i;
 
 public interface Rectangle {
-    public Vector2i getTopLeftCorner();
-    public Vector2i getBottomRightCorner();
+    Vector2i getTopLeftCorner();
+    Vector2i getBottomRightCorner();
 
-    public default int maxX() {
+    default int maxX() {
         return getBottomRightCorner().getX();
     }
 
-    public default int minX() {
+    default int minX() {
         return getTopLeftCorner().getX();
     }
 
-    public default int maxY() {
+    default int maxY() {
         return getTopLeftCorner().getY();
     }
 
-    public default int minY() {
+    default int minY() {
         return getBottomRightCorner().getY();
     }
 }
