f08fc5701e922089213302e8f231541d3d9043d1
Liam <liamreadsspam@gmail.com>
2020-09-26 01:42:55 -0300

    Move town plot stuff

--- src/main/java/com/atherys/towns/model/entity/Plot.java
+++ src/main/java/com/atherys/towns/model/entity/Plot.java
@@ -26,30 +26,6 @@ public class Plot implements Rectangle, Identifiable<Long> {
     @Convert(converter = Vector2iConverter.class)
     private Vector2i neCorner;
 
-    @ManyToOne(fetch = FetchType.EAGER)
-    private Resident owner;
-
-
-    @Convert(converter = PermissionConverter.class)
-    @ElementCollection(fetch = FetchType.EAGER)
-    private Set<WorldPermission> friendPermissions = new HashSet<>();
-
-    @Convert(converter = PermissionConverter.class)
-    @ElementCollection(fetch = FetchType.EAGER)
-    private Set<WorldPermission> townPermissions = new HashSet<>();
-
-    @Convert(converter = PermissionConverter.class)
-    @ElementCollection(fetch = FetchType.EAGER)
-    private Set<WorldPermission> allyPermissions = new HashSet<>();
-
-    @Convert(converter = PermissionConverter.class)
-    @ElementCollection(fetch = FetchType.EAGER)
-    private Set<WorldPermission> enemyPermissions = new HashSet<>();
-
-    @Convert(converter = PermissionConverter.class)
-    @ElementCollection(fetch = FetchType.EAGER)
-    private Set<WorldPermission> neutralPermissions = new HashSet<>();
-
     @Version
     private int version;
 
@@ -97,46 +73,6 @@ public class Plot implements Rectangle, Identifiable<Long> {
     @Override
     public void setBottomRightCorner(Vector2i point) {
         this.neCorner = point;
-    }
-
-    public Set<WorldPermission> getFriendPermissions() {
-        return this.friendPermissions;
-    }
-
-    public void setFriendPermissions(Set<WorldPermission> friendPermissions) {
-        this.friendPermissions = friendPermissions;
-    }
-
-    public Set<WorldPermission> getTownPermissions() {
-        return this.townPermissions;
-    }
-
-    public void setTownPermissions(Set<WorldPermission> townPermissions) {
-        this.townPermissions = townPermissions;
-    }
-
-    public Set<WorldPermission> getAllyPermissions() {
-        return this.allyPermissions;
-    }
-
-    public void setAllyPermissions(Set<WorldPermission> allyPermissions) {
-        this.allyPermissions = allyPermissions;
-    }
-
-    public Set<WorldPermission> getEnemyPermissions() {
-        return this.enemyPermissions;
-    }
-
-    public void setEnemyPermissions(Set<WorldPermission> enemyPermissions) {
-        this.enemyPermissions = enemyPermissions;
-    }
-
-    public Set<WorldPermission> getNeutralPermissions() {
-        return this.neutralPermissions;
-    }
-
-    public void setNeutralPermissions(Set<WorldPermission> neutralPermissions) {
-        this.neutralPermissions = neutralPermissions;
     }
 
     protected int getVersion() {
--- src/main/java/com/atherys/towns/model/entity/TownPlot.java
+++ src/main/java/com/atherys/towns/model/entity/TownPlot.java
@@ -1,9 +1,13 @@
 package com.atherys.towns.model.entity;
 
+import com.atherys.towns.api.permission.world.WorldPermission;
+import com.atherys.towns.persistence.converter.PermissionConverter;
 import com.atherys.towns.persistence.converter.TextConverter;
 import org.spongepowered.api.text.Text;
 
 import javax.persistence.*;
+import java.util.HashSet;
+import java.util.Set;
 
 @Entity
 public class TownPlot extends Plot {
@@ -17,6 +21,26 @@ public class TownPlot extends Plot {
 
     @ManyToOne(fetch = FetchType.EAGER)
     private Resident owner;
+
+    @Convert(converter = PermissionConverter.class)
+    @ElementCollection(fetch = FetchType.EAGER)
+    private Set<WorldPermission> friendPermissions = new HashSet<>();
+
+    @Convert(converter = PermissionConverter.class)
+    @ElementCollection(fetch = FetchType.EAGER)
+    private Set<WorldPermission> townPermissions = new HashSet<>();
+
+    @Convert(converter = PermissionConverter.class)
+    @ElementCollection(fetch = FetchType.EAGER)
+    private Set<WorldPermission> allyPermissions = new HashSet<>();
+
+    @Convert(converter = PermissionConverter.class)
+    @ElementCollection(fetch = FetchType.EAGER)
+    private Set<WorldPermission> enemyPermissions = new HashSet<>();
+
+    @Convert(converter = PermissionConverter.class)
+    @ElementCollection(fetch = FetchType.EAGER)
+    private Set<WorldPermission> neutralPermissions = new HashSet<>();
 
     public Town getTown() {
         return town;
@@ -40,5 +64,45 @@ public class TownPlot extends Plot {
 
     public void setOwner(Resident owner) {
         this.owner = owner;
+    }
+
+    public Set<WorldPermission> getFriendPermissions() {
+        return this.friendPermissions;
+    }
+
+    public void setFriendPermissions(Set<WorldPermission> friendPermissions) {
+        this.friendPermissions = friendPermissions;
+    }
+
+    public Set<WorldPermission> getTownPermissions() {
+        return this.townPermissions;
+    }
+
+    public void setTownPermissions(Set<WorldPermission> townPermissions) {
+        this.townPermissions = townPermissions;
+    }
+
+    public Set<WorldPermission> getAllyPermissions() {
+        return this.allyPermissions;
+    }
+
+    public void setAllyPermissions(Set<WorldPermission> allyPermissions) {
+        this.allyPermissions = allyPermissions;
+    }
+
+    public Set<WorldPermission> getEnemyPermissions() {
+        return this.enemyPermissions;
+    }
+
+    public void setEnemyPermissions(Set<WorldPermission> enemyPermissions) {
+        this.enemyPermissions = enemyPermissions;
+    }
+
+    public Set<WorldPermission> getNeutralPermissions() {
+        return this.neutralPermissions;
+    }
+
+    public void setNeutralPermissions(Set<WorldPermission> neutralPermissions) {
+        this.neutralPermissions = neutralPermissions;
     }
 }
